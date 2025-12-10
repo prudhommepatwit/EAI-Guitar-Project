130679156d52d620530b551194d5cc169cd5e853
Avery Guion <144967081+guiona3@users.noreply.github.com>
2025-12-10 13:07:20 -0500

    Reverted predict.py library

--- predict.py
+++ predict.py
@@ -11,49 +11,49 @@ model = tf.keras.models.load_model(MODEL_PATH, custom_objects=None, compile=True
 
 #Update dictionary to take in (42? 32?) unique IDs and determine which note is which
 id_to_note = {
-    "A2" : "A2",
-    "A3" : "A3",
-    "A4" : "A4",
-    "Asharp2" : "A#2",
-    "Asharp3" : "A#3",
-    "Asharp4" : "A#4",
-    "B2" : "B2",
-    "B3" : "B3",
-    "B4" : "B4",
-    "C3" : "C3",
-    "C4" : "C4",
-    "C5" : "C5",
-    "Csharp3" : "C#3",
-    "Csharp4" : "C#4",
-    "Csharp5" : "C#5",
-    "D2" : "D2",
-    "D3" : "D3",
-    "D4" : "D4",
-    "D5" : "D5",
-    "Dsharp2" : "D#2",
-    "Dsharp3" : "D#3",
-    "Dsharp4" : "D#4",
-    "Dsharp5" : "D#5",
-    "E3" : "E2",
-    "E3" : "E3",
-    "E3" : "E4",
-    "E3" : "E5",
-    "F3" : "F2",
-    "F3" : "F3",
-    "F3" : "F4",
-    "F3" : "F5",
-    "Fsharp2" : "F#2",
-    "Fsharp3" : "F#3",
-    "Fsharp4" : "F#4",
-    "Fsharp5" : "F#5",
-    "G2" : "G2",
-    "G3" : "G3",
-    "G4" : "G4",
-    "G5" : "G5",
-    "Csharp2" : "G#2",
-    "Csharp3" : "G#3",
-    "Csharp4" : "G#4",
-    "Csharp5" : "G#5"
+    0 : "A2",
+    1 : "A3",
+    2 : "A4",
+    3 : "A#2",
+    4 : "A#3",
+    5 : "A#4",
+    6 : "B2",
+    7 : "B3",
+    8 : "B4",
+    9 : "C3",
+    10 : "C4",
+    11 : "C5",
+    12 : "C#3",
+    13 : "C#4",
+    14 : "C#5",
+    15 : "D2",
+    16 : "D3",
+    17 : "D4",
+    18 : "D5",
+    19 : "D#2",
+    20 : "D#3",
+    21 : "D#4",
+    22 : "D#5",
+    23 : "E2",
+    24 : "E3",
+    25 : "E4",
+    26 : "E5",
+    27 : "F2",
+    28 : "F3",
+    29 : "F4",
+    30 : "F5",
+    31 : "F#2",
+    32 : "F#3",
+    33 : "F#4",
+    34 : "F#5",
+    35 : "G2",
+    36 : "G3",
+    37 : "G4",
+    38 : "G5",
+    39 : "G#2",
+    40 : "G#3",
+    41 : "G#4",
+    42 : "G#5"
 }
 
 def convert_webm_to_wav(webm_path, wav_path):
@@ -86,8 +86,9 @@ def noteClassify(userAudioPath, target_sr=16000, max_len=16000):
     outputs = model(inputs)
     predicted_id = np.argmax(outputs, axis=1)[0]
 
-    #Temporarily returning just the id number
-    #Casted from np.int32 to int
-    return int(predicted_id)
+    # Temporarily returning just the id number
+    # Casted from np.int32 to int
+    # return int(predicted_id)
+  
 
     return id_to_note[int(predicted_id)]
