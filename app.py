9b4699313871f57eaa86c1a18ea90dec2ebe194d
prudhommepatwit <prudhommep@wit.edu>
2025-12-11 11:13:00 -0500

    fixed async mode

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app, async_mode="threading")
+socketio = SocketIO(app, async_mode = 'gevent')
 
 
 @app.route('/')
